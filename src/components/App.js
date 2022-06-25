d8872d91274a36d477a9c1a70fc8f614a55aaf51
casalima <casalima@hotmail.com>
2022-06-24 21:20:47 -0500

    after adding semantic and stablish comm to github

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
+// sca - remove
 //import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+
+// sca - Adding my components
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
 
 function App() {
   return (
+    <div>
+      <Header />
+      {/* <AddContact />
+      <ContactList /> */}
+    </div>
 
-    <div>Hello World</div>
+    //<div>Hello World</div>
 
     // <div className="App">
     //   <header className="App-header">
